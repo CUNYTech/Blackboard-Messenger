0fff10a23674986ec361b35dd3fbbe338e971586
William Wu <will.go.code@gmail.com>
2017-03-03 01:16:59 -0500

    Updated iOS frontend to match Android front end.

--- iOS App/BlackboardMessenger/BlackboardMessenger/LoginViewController.swift
+++ iOS App/BlackboardMessenger/BlackboardMessenger/LoginViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+	@IBOutlet weak var usernameField: UITextField!
+	@IBOutlet weak var passwordField: UITextField!
+	@IBAction func loginButton(_ sender: UIButton) {
+		
+	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
