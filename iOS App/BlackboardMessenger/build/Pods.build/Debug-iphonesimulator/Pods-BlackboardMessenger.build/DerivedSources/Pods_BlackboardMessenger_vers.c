1ed2964754ac753691953c188e09b69777b78d9b
William Wu <williamwu0013@gmail.com>
2017-03-06 20:51:52 -0500

    Updated xcode project to use Alamofire.

---
+++ iOS App/BlackboardMessenger/build/Pods.build/Debug-iphonesimulator/Pods-BlackboardMessenger.build/DerivedSources/Pods_BlackboardMessenger_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_BlackboardMessengerVersionString[];
+ extern const double Pods_BlackboardMessengerVersionNumber;
+
+ const unsigned char Pods_BlackboardMessengerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_BlackboardMessenger  PROJECT:Pods-1" "\n";
+ const double Pods_BlackboardMessengerVersionNumber __attribute__ ((used)) = (double)1.;
